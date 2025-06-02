328999062eb6f6a7428447d8d2e24cdb5066b92d
MaxOuvrard <118462372+MaxOuvrard@users.noreply.github.com>
2025-06-02 23:20:49 +0200

    Initial commit for part5

--- src/main/java/factory/Factory.java
+++ src/main/java/factory/Factory.java
@@ -1,20 +1,52 @@
 package factory;
 
-import action.*;
-
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import action.Action;
+
 public class Factory {
-    private static HashMap<String, Class> classHashMap = new HashMap<>();
+    private static HashMap<String, Class<? extends Action>> classHashMap = new HashMap<>();
 
     static {
-        classHashMap.put("loginServlet.do", LoginAction.class);
-        classHashMap.put("logoutServlet.do", LogoutAction.class);
-        classHashMap.put("updateAttributesServlet.do",ActionUn.class);
-        classHashMap.put("debut_servlet.do",ActionDebut.class);
+        try {
+            InputStream xmlStream = Factory.class.getClassLoader().getResourceAsStream("actions.xml");
+            if (xmlStream == null) {
+                throw new RuntimeException("actions.xml non trouvé dans les ressources");
+            }
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlStream);
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("action");
+            for (int i = 0; i < nList.getLength(); i++) {
+                Node node = nList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element elem = (Element) node;
+                    String name = elem.getAttribute("name");
+                    String className = elem.getAttribute("class");
+                    Class<? extends Action> clazz = (Class<? extends Action>) Class.forName(className);
+                    classHashMap.put(name, clazz);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public static Action get(String className) throws InstantiationException, IllegalAccessException {
-        return (Action) classHashMap.get(className).newInstance();
+    public static Action get(String className) throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Class<? extends Action> clazz = classHashMap.get(className);
+        if (clazz == null) throw new IllegalArgumentException("Action non trouvée : " + className);
+        return clazz.getDeclaredConstructor().newInstance();
     }
 }
---
+++ src/main/webapp/vide.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Vide</title>
+</head>
+<body>
+<p>Champs vides, merci de remplir les champs</p>
+</body>
+</html>
